cfc9387c909b7e9e641da63c74757deee13d9f6b
06066060606060 <xmicky+git@hotmail.fr>
2023-01-24 20:57:53 +0400

    tags

--- resources/views/masterplans_post.blade.php
+++ resources/views/masterplans_post.blade.php
@@ -8,120 +8,58 @@
                        <div class="relative px-4 py-8 mx-auto my-8">
                            <div class="grid items-start grid-cols-1 gap-8 lg:grid-cols-2">
                                <div class="grid grid-cols-1 gap-4">
-
                                    <div class="w-full">
-                                           <div class="swiper-slide">
-                                               @php $image0 =  $item->image[0] ?? null; @endphp
-                                               <img class="object-cover w-full" src="./storage/{{ $image0 }}"
-                                                   alt="" onerror="this.src='/img/empty.png'">
-                                           </div>
+                                       @php $image0 =  $item->image[0] ?? null; @endphp
+                                       <img class="object-cover w-full" src="./storage/{{ $image0 }}" alt=""
+                                           onerror="this.src='/img/empty.png'">
                                    </div>
 
                                </div>
 
                                <div class="sticky top-0">
-                                   <div class="flex">
+                                   <div class="flex justify-between pb-8">
+                                       <h1 class="text-2xl font-bold">
+                                           {{ $item->title }} | {{ $item->author }} | {{ $item->city }}
+                                       </h1>
+                                   </div>
+                                   <div class="flex pb-8">
                                        <button
-                                           class="px-3 py-1 text-xs font-medium tracking-wide text-white bg-black rounded-md">
-                                          {{ $item->category }}
+                                           class="px-3 py-1 mx-1 text-xs font-medium tracking-wide text-white bg-blue-800 rounded-md">
+                                           {{ $item->status }}
                                        </button>
+
+                                       <button
+                                           class="px-3 py-1 mx-1 text-xs font-medium tracking-wide text-white bg-black rounded-md">
+                                           <a href="masterplans"> {{ $item->category }}</a>
+                                       </button>
+                                       <button
+                                           class="px-3 py-1 mx-1 text-xs font-medium tracking-wide text-white bg-black rounded-md">
+                                           {{ $item->size }}
+                                       </button>
+                                       @foreach ($item->tags as $tag)
+                                           <button
+                                               class="px-3 py-1 mx-1 text-xs font-medium tracking-wide text-white bg-black rounded-md">
+                                               {{ $tag }}
+                                           </button>
+                                       @endforeach
+
                                    </div>
 
-                                   <div class="flex justify-between mt-8">
+                                   <div class="block">
                                        <div>
-                                           <h1 class="text-2xl font-bold">
-                                               {{ $item->title }}
-                                           </h1>
-                                           <a class="py-2 mb-4 text-sm font-bold text-gray-600"><span
-                                                   class="text-black">
-                                                   {{ $item->city }}</span> </a>
-                                       </div>
-                                       <p class="text-lg font-bold"> </p>
-                                   </div>
-
-                                   <details class="group relative mt-4 [&_summary::-webkit-details-marker]:hidden">
-                                       <summary class="block">
-                                           <div>
-                                               <div class="prose max-w-none group-open:hidden">
-                                                   <p>
-                                                       {{ $item->description }}
-                                                   </p>
-                                               </div>
-
-                                               <span
-                                                   class="mt-4 text-sm font-medium underline cursor-pointer group-open:absolute group-open:bottom-0 group-open:left-0 group-open:mt-0">
-                                                   En savoir plus
-                                               </span>
+                                           <div class="text-gray-700 max-w-none group-open:hidden">
+                                               <p>
+                                                   {{ $item->description }}
+                                               </p>
                                            </div>
-                                       </summary>
 
-                                       <div class="pb-6 prose max-w-none">
-                                           <p>
-                                               {{ $item->description }}
-                                           </p>
                                        </div>
-                                   </details>
-
+                                   </div>
 
                                    <fieldset>
-                                       <legend class="mb-1 text-sm font-medium">Details:</legend>
-
-                                       <div class="flow-root">
-                                           <div class="-m-0.5 flex flex-wrap">
-
-
-                                               <label for="color_tt" class="p-0.5">
-                                                   <span
-                                                       class="inline-block px-3 py-1 text-xs font-medium bg-white border rounded-full group">
-                                                       Terrasse
-                                                   </span>
-                                               </label>
-
-
-
-                                               <label for="color_tt" class="p-0.5">
-                                                   <span
-                                                       class="inline-block px-3 py-1 text-xs font-medium bg-white border rounded-full group">
-                                                       Balcon
-                                                   </span>
-                                               </label>
-
-
-                                               <label for="color_tt" class="p-0.5">
-                                                   <span
-                                                       class="inline-block px-3 py-1 text-xs font-medium bg-white border rounded-full group">
-                                                       Garage
-                                                   </span>
-                                               </label>
-
-
-
-
-                                               <label for="color_tt" class="p-0.5">
-                                                   <span
-                                                       class="inline-block px-3 py-1 text-xs font-medium bg-white border rounded-full group">
-                                                       Parking
-                                                   </span>
-                                               </label>
-
-
-
-                                               <label for="color_tt" class="p-0.5">
-                                                   <span
-                                                       class="inline-block px-3 py-1 text-xs font-medium bg-white border rounded-full group">
-                                                       Piscine
-                                                   </span>
-                                               </label>
-
-
-
-                                               <label for="color_tt" class="p-0.5">
-                                                   <span
-                                                       class="inline-block px-3 py-1 text-xs font-medium bg-white border rounded-full group">
-                                                       Jardin
-                                                   </span>
-                                               </label>
-                                           </div>
+                                       <legend class="pt-4 pb-2 mb-1 font-medium">Sources:</legend>
+                                       <div class="flex flex-wrap text-sm">
+                                           {{ $item->credits }}
                                        </div>
                                    </fieldset>
 
@@ -131,7 +69,18 @@
                        </div>
                    </section>
                @empty
-                   <h1 class="text-2xl font-bold">Empty</h1>
+                   <section>
+                       <div class="relative px-4 py-8 mx-auto my-8">
+                           <div class="grid items-start grid-cols-1 gap-8 lg:grid-cols-2">
+                               <div class="grid grid-cols-1 gap-4">
+                                   <div class="w-full">
+                                       <h1 class="text-2xl font-bold">Empty</h1>
+                                   </div>
+                               </div>
+                           </div>
+
+                       </div>
+                   </section>
                @endforelse
 
 
