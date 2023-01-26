51c8f88ecd480b33587e4380da8b72e3693e4001
06066060606060 <xmicky+git@hotmail.fr>
2023-01-26 18:19:52 +0400

    33

--- resources/views/parts/neighbour.blade.php
+++ resources/views/parts/neighbour.blade.php
@@ -106,7 +106,7 @@
 
         <section>
             <div class="max-w-screen-xl px-4 py-8 mx-auto sm:px-6 sm:py-12 lg:px-8">
-                <ul id="boucle" class="grid gap-4 mt-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+                <ul id="boucle" class="grid gap-4 mt-8 sm:grid-cols-2 md:grid-cols-3 ">
 
                 </ul>
             </div>
