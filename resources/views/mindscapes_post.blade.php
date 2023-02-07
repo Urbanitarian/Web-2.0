d56275181c9b2a0e75d9d4e3e3269ba6a2d61e40
06066060606060 <xmicky+git@hotmail.fr>
2023-02-07 20:26:22 +0400

    fix

--- resources/views/mindscapes_post.blade.php
+++ resources/views/mindscapes_post.blade.php
@@ -23,10 +23,10 @@
                             @endforeach
                         </div>
                     </div>
-                    <div>
+                    <div class="">
                         @php $img7 = $mindscape->image ?? null; @endphp
 
-                        <img class="object-cover h-64 mb-6 rounded shadow-lg lg:h-96"
+                        <img class="object-cover h-64 mb-6 rounded shadow-lg lg:h-96 w-full"
                             src="{{ asset('storage/' . $img7) }}"alt="00" onerror="this.src='./img/empty.png'" />
                     </div>
                     <div class="flex justify-between">
