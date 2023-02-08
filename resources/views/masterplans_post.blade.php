c373cadfae2232877412ce3f3d862b78f6a5b508
06066060606060 <xmicky+git@hotmail.fr>
2023-02-09 00:14:20 +0400

    azaza

--- resources/views/masterplans_post.blade.php
+++ resources/views/masterplans_post.blade.php
@@ -331,7 +331,7 @@
            decimalString[0] = parseFloat(decimalString[0]).toFixed(6);
            decimalString[1] = parseFloat(decimalString[1]).toFixed(6);
 
-           let mymap = L.map('mastermap').setView([decimalString[0], decimalString[1]], 16);
+           let mymap = L.map('mastermap').setView([decimalString[0], decimalString[1]], 18);
            osmLayer = L.tileLayer(
                'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
                    maxZoom: 19,
