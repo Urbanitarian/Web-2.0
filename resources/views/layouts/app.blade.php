d2b4d6be741d357c2119360bbdf270550cbf25f4
06066060606060 <xmicky+git@hotmail.fr>
2023-02-26 17:37:35 +0400

    event

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,46 +6,55 @@
 </head>
 
 <body>
- @php
+    @php
         $topbar_text = Setting::get('topbar_text');
-        @endphp
-        @if ($topbar_text != null)
-       <div class="marquee py-2">{{ $topbar_text }}  &nbsp;   &nbsp; {{ $topbar_text }}  &nbsp;   &nbsp; {{ $topbar_text }}  &nbsp; &nbsp; {{ $topbar_text }}   &nbsp; &nbsp;  {{ $topbar_text }}  &nbsp;   &nbsp; {{ $topbar_text }}  &nbsp;   &nbsp; {{ $topbar_text }}  &nbsp; &nbsp; {{ $topbar_text }}</div>
-        @endif
-        @include('parts.navbar')
+    @endphp
+    @if ($topbar_text != null)
+        <div class="marquee py-2 hidden">{{ $topbar_text }} &nbsp; &nbsp; {{ $topbar_text }} &nbsp; &nbsp; {{ $topbar_text }}
+            &nbsp; &nbsp; {{ $topbar_text }} &nbsp; &nbsp; {{ $topbar_text }} &nbsp; &nbsp; {{ $topbar_text }} &nbsp;
+            &nbsp; {{ $topbar_text }} &nbsp; &nbsp; {{ $topbar_text }}</div>
+    @endif
+    @include('parts.navbar')
     <main>
         @yield('main')
     </main>
     <footer>
         @include('parts.footer')
     </footer>
-    
+
     @vite('resources/js/app.js')
-        <script src="https://unpkg.com/swiper@6.8.4/swiper-bundle.min.js"></script>
+    <script src="https://unpkg.com/swiper@6.8.4/swiper-bundle.min.js"></script>
 
 
 </body>
 <style>
-.marquee {
-  color: white;
-  overflow: hidden;
-  background: black;
-}
+    .marquee {
+        color: white;
+        overflow: hidden;
+        background: black;
+    }
 </style>
 <script>
-$('.marquee').marquee({
-    //speed in milliseconds of the marquee
-    duration: 15000,
-    //gap in pixels between the tickers
-    gap: 20,
-    //time in milliseconds before the marquee will start animating
-    delayBeforeStart: 0,
-    //'left' or 'right'
-    direction: 'left',
-    //true or false - should the marquee be duplicated to show an effect of continues flow
-    duplicated: true
+   addEventListener('load', function() {
+        $('.marquee').removeClass('hidden');
+ $('.marquee').marquee({
+        //speed in milliseconds of the marquee
+        duration: 15000,
+        //gap in pixels between the tickers
+        gap: 20,
+        //time in milliseconds before the marquee will start animating
+        delayBeforeStart: 0,
+        //'left' or 'right'
+        direction: 'left',
+        //true or false - should the marquee be duplicated to show an effect of continues flow
+        duplicated: true
+
+    });
+
+    }, false);
+
+
    
-});
 </script>
 
 </html>
