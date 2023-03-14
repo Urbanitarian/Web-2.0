761f53d3d7fb1af530edb83893d4a7429697a4c8
06066060606060 <xmicky+git@hotmail.fr>
2023-03-14 16:28:54 +0400

    clean footer

--- resources/views/layouts/app2.blade.php
+++ resources/views/layouts/app2.blade.php
@@ -11,7 +11,71 @@
         @yield('main')
     </main>
     <footer>
-        @include('parts.footer')
+    <div aria-label="Site Footer" class="border-t">
+    <div class="max-w-5xl px-4 pt-16 pb-8 mx-auto sm:px-6 lg:px-8">
+      <div class="flex justify-center text-teal-600">
+        <img src="{{ asset('img/logo.png') }}" alt="logo" class="w-12 h-12" />
+      </div>
+
+      <nav aria-label="Footer Nav" class="mt-12">
+        <ul class="flex flex-wrap justify-center gap-6 md:gap-8 lg:gap-12">
+          <li class="duration-300 hover:scale-110">
+            <a class="text-gray-700 transition hover:text-gray-900" href="about">
+              About
+            </a>
+          </li>
+
+           <li class="duration-300 hover:scale-110">
+            <a class="text-gray-700 transition hover:text-gray-900" href="subscribe">
+              Subscribe
+            </a>
+          </li>
+
+          <li class="duration-300 hover:scale-110">
+            <a class="text-gray-700 transition hover:text-gray-900" href="contact">
+              Contact
+            </a>
+          </li>
+
+         <li class="duration-300 hover:scale-110">
+            <a class="text-gray-700 transition hover:text-gray-900" href="people">
+              Team
+            </a>
+          </li>
+
+         {{-- <li class="duration-300 hover:scale-110">
+            <a class="text-gray-700 transition hover:text-gray-900" href="legal">
+              Terms of use
+            </a>
+          </li>
+
+          <li class="duration-300 hover:scale-110">
+            <a class="text-gray-700 transition hover:text-gray-900" href="confidentialite">
+              Privacy policy
+            </a>
+          </li> --}}
+
+          <li class="duration-300 hover:scale-110">
+            <a class="text-gray-700 transition hover:text-gray-900" href="cookies">
+              Cookie policy
+            </a>
+          </li>
+
+            <li class="duration-300 hover:scale-110">
+            <a class="text-gray-700 transition hover:text-gray-900" href="association">
+              Association
+            </a>
+          </li> 
+        </ul>
+      </nav>
+
+    </div>
+    <div class="flex justify-center pb-4 text-gray-500">
+       <a href="admin"> <p class="flex flex-col items-center text-xs">
+       <span> &copy; All Rights Reserved. Urbanitarian</span>
+       <span>  All images are &copy; each office/photographer mentioned.</span>
+      </p></a>
+  </div>
     </footer>
 
     @vite('resources/js/app.js')
