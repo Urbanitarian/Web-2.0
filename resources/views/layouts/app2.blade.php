67f1f7bbf8b779743a635fcb98ee4812fc991b75
06066060606060 <xmicky+git@hotmail.fr>
2023-03-14 16:26:18 +0400

    up

--- resources/views/layouts/app2.blade.php
+++ resources/views/layouts/app2.blade.php
@@ -6,17 +6,6 @@
 </head>
 
 <body>
-    @php
-        $topbar_text = Setting::get('topbar_text');
-    @endphp
-    @if ($topbar_text != null)
-    <div class="hidden md:block">
-        <div class="hidden py-2 select-none marquee">{{ $topbar_text }} &nbsp; &nbsp; {{ $topbar_text }} &nbsp; &nbsp; {{ $topbar_text }}
-            &nbsp; &nbsp; {{ $topbar_text }} &nbsp; &nbsp; {{ $topbar_text }} &nbsp; &nbsp; {{ $topbar_text }} &nbsp;
-            &nbsp; {{ $topbar_text }} &nbsp; &nbsp; {{ $topbar_text }}
-        </div>
-       </div>
-    @endif
     @include('parts.navbar2')
     <main>
         @yield('main')
@@ -26,38 +15,8 @@
     </footer>
 
     @vite('resources/js/app.js')
-    <script src="https://unpkg.com/swiper@6.8.4/swiper-bundle.min.js"></script>
-
-
 </body>
-<style>
-    .marquee {
-        color: white;
-        overflow: hidden;
-        background: black;
-    }
-</style>
-<script>
-   addEventListener('load', function() {
-        $('.marquee').removeClass('hidden');
- $('.marquee').marquee({
-        //speed in milliseconds of the marquee
-        duration: 15000,
-        //gap in pixels between the tickers
-        gap: 20,
-        //time in milliseconds before the marquee will start animating
-        delayBeforeStart: 0,
-        //'left' or 'right'
-        direction: 'left',
-        //true or false - should the marquee be duplicated to show an effect of continues flow
-        duplicated: true
-
-    });
-
-    }, false);
 
 
-   
-</script>
 
 </html>
